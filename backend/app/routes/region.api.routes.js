f4ea057dbbb374acb528a9f81319563413065a2b
machi2001 <ckdwh277@gmail.com>
2025-04-29 01:23:39 +0900

    BE: CCTV 관련 API 구현 및 지역 추가, 수정, 삭제 API 구현

--- backend/app/routes/region.api.routes.js
+++ backend/app/routes/region.api.routes.js
@@ -2,5 +2,12 @@ const regionController = require('../../app/controllers/region.controller');
 
 module.exports = function(app, UserApiRoutes) {
   // 지역 정보 조회 API
-  UserApiRoutes.route('/regions').get(regionController.getRegions);
+  UserApiRoutes.route('/regions')
+    .get(regionController.getRegions)
+    .post(regionController.createRegion);
+  
+  // 지역 관리 API
+  UserApiRoutes.route('/regions/:regionId')
+    .put(regionController.updateRegion)
+    .delete(regionController.deleteRegion);
 };
